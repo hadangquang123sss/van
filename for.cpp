631d6a5466dd2894cd96ddf63baacde5d1752241
hadangquang <hadangquang1408@gmail.com>
2021-03-12 20:01:47 +0700

    first commit

---
+++ for.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	    int  a;
+	    int  b;
+	    printf("nhap a = " );
+	    scanf("%d",&a);
+	    printf("nhap b = " );
+	    scanf("%d",&b);
+	    
+	    
+	for(int i = a ; i < b ; i++){
+		printf("%d\n",i);
+		}
+
+ return 0 ;
+}
+
